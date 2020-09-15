775bba28e39de93b1be89085a65900c46d0536bb
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-09-15 09:54:52 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+
+console.log('it is working');
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+
+
+for (var i = 0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(e){
+        let mode = this.dataset.mode;
+        console.log('Option:', mode);
+       
+        setTheme(mode);
+      
+    })
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue-theme.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'green-theme.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple-theme.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
+
+
+
+
+
+
