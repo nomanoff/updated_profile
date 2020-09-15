7a7340effd8ec0e3c72d03df23652f7520575c81
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-09-15 13:03:02 +0900

    typint effect added

--- main.js
+++ main.js
@@ -45,6 +45,34 @@ function setTheme(mode){
 
 
 
+//////////////////////////TYPING EFFECT////////////////
+
+const message = ["Who am I?"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = " ";
+
+function type(){
+
+   
+    if(count === message.length){
+        count = 0;
+    }
+
+    currentText = message[count];
+    letter = currentText.slice(0, ++index);
+    console.log(letter);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 200);
+};
+
+type();
 
 
 
